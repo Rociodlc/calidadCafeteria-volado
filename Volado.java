c0171c018fb3a542d0ff1ab62fbe82048b0794f1
Rociodlc <124368756+Rociodlc@users.noreply.github.com>
2023-05-28 08:00:58 -0600

    Add files via upload

---
+++ Volado.java
@@ -0,0 +1,27 @@
+
+
+public static void main(String[]args){
+
+    int[] volado = new int[3];
+    final int AGUILA = 1;
+    final int SOL = 2;
+    final int LANZAMIENTOS = 100;
+
+    volado[AGUILA]
+
+                }
+  for(int i = 0; 1 < LANZAMIENTOS; i++){
+      lanzamiento = random.nextInt(2) + 1;
+      volado[lanamiento] = volado[lanzamiento] + 1;
+      if (LANZAMIENTO == AGUILA){
+           volado[AGUILA] + 1;
+        }
+           if(lanzamiento == SOL){
+           volado[SOL] = volado[SOL]+ 1;
+        }
+        System.out.println("cayo" + volado[AGUILA] + "veces AGUILA");
+        System.out.println("cayo" + volado[SOL] + "veces SOL");
+
+
+
+        }
